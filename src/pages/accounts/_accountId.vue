xymopen/CuckooPlus

feat: Adopt layouts

src/pages/accounts/_accountId.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="account-container">
+  <DefaultLayout class="account-container">
     <account-header />
-  </div>
+  </DefaultLayout>
 </template>
 
 <route>
@@ -14,10 +14,12 @@
 import { Vue, Component } from 'vue-property-decorator'
 import { } from 'vuex-class'
 import AccountHeader from './components/AccountHeader.vue'
+import DefaultLayout from '@/layouts/default/index.vue'
 
 @Component({
   components: {
-    'account-header': AccountHeader
+    'account-header': AccountHeader,
+    DefaultLayout
   }
 })
 class Accounts extends Vue {

src/pages/statuses/_statusId.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="statuses-page-container" v-loading="!status">
+  <DefaultLayout class="statuses-page-container" v-loading="!status">
     <status-card class="status-card-container" v-if="status" :status="status" />
-  </div>
+  </DefaultLayout>
 </template>
 
 <route>
@@ -15,10 +15,12 @@ import { Vue, Component, Watch } from 'vue-property-decorator'
 import { State, Action } from 'vuex-class'
 import { mastodonentities } from '@/interface'
 import StatusCard from '@/components/StatusCard/index.vue'
+import DefaultLayout from '@/layouts/default/index.vue'
 
 @Component({
   components: {
     'status-card': StatusCard,
+    DefaultLayout
   }
 })
 class Statuses extends Vue {

xymopen/Cuckoo.Plus

src/pages/accounts/_accountId.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="account-container">
-    <account-header />
-  </div>
+  <DefaultLayout>
+    <div class="account-container">
+      <account-header />
+    </div>
+  </DefaultLayout>
 </template>
 
 <route>
@@ -14,10 +16,12 @@
 import { Vue, Component } from 'vue-property-decorator'
 import { } from 'vuex-class'
 import AccountHeader from './components/AccountHeader.vue'
+import DefaultLayout from '@/layouts/default/index.vue'
 
 @Component({
   components: {
-    'account-header': AccountHeader
+    'account-header': AccountHeader,
+    DefaultLayout
   }
 })
 class Accounts extends Vue {

src/pages/statuses/_statusId.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="statuses-page-container" v-loading="!status">
-    <status-card class="status-card-container" v-if="status" :status="status" />
-  </div>
+  <DefaultLayout>
+    <div class="statuses-page-container" v-loading="!status">
+      <status-card class="status-card-container" v-if="status" :status="status" />
+    </div>
+  </DefaultLayout>
 </template>
 
 <route>
@@ -15,10 +17,12 @@ import { Vue, Component, Watch } from 'vue-property-decorator'
 import { State, Action } from 'vuex-class'
 import { mastodonentities } from '@/interface'
 import StatusCard from '@/components/StatusCard/index.vue'
+import DefaultLayout from '@/layouts/default/index.vue'
 
 @Component({
   components: {
     'status-card': StatusCard,
+    DefaultLayout
   }
 })
 class Statuses extends Vue {